74517892f930561f0d019becdee5aa54fc1d5ade
NextCodigos <nebulanemesis376@gmail.com>
2024-06-11 00:05:49 +0200

    Hello world con bordes de varios colores

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ document.querySelector("h1").style.textAlign = "center";
 document.querySelector("h1").style.border = "6px solid black";
 document.querySelector("h1").style.borderRight = "9px solid red";
 document.querySelector("h1").style.borderLeft = "5px solid red";
+document.querySelector("h1").style.borderTop = "5px solid violet";
+document.querySelector("h1").style.borderBottom = "5px solid green";
