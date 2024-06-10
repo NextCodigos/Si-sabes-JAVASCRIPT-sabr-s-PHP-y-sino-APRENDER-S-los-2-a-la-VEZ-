750f503f0d7c123515c80d21942699d1d83992cb
NextCodigos <nebulanemesis376@gmail.com>
2024-06-11 00:11:25 +0200

    Pongo css,javascript en html

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ document.querySelector("h1").style.textAlign = "center";
 document.querySelector("h1").style.border = "6px solid black";
 document.querySelector("h1").style.borderRight = "9px solid red";
 document.querySelector("h1").style.borderLeft = "5px solid red";
+document.querySelector("h1").style.borderTop = "5px solid blue";
+document.querySelector("h1").style.borderBottom = "5px solid green";
+document.querySelector("body").innerHTML = "<h1>Hello World!</h1>";
+document.querySelector("h1").innerHTML = "Hello World!";
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.backgroundColor = "blue";
+document.querySelector("h1").style.padding = "10px";
+document.querySelector("h1").style.textAlign = "center";
+
+document.querySelector("h1").style.border = "6px solid black";
+document.querySelector("h1").style.borderRight = "9px solid red";
+document.querySelector("h1").style.borderLeft = "5px solid red";
 document.querySelector("h1").style.borderTop = "5px solid violet";
 document.querySelector("h1").style.borderBottom = "5px solid green";
