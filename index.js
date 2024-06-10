c8f82b0eb72acf462135b7139d770a7dc50c486e
NextCodigos <nebulanemesis376@gmail.com>
2024-06-10 23:59:15 +0200

    Hello world con borde negro

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector("body").innerHTML = "<h1>Hello World!</h1>";
+document.querySelector("h1").innerHTML = "Hello World!";
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.backgroundColor = "blue";
+document.querySelector("h1").style.padding = "10px";
+document.querySelector("h1").style.textAlign = "center";
+
+document.querySelector("h1").style.border = "6px solid black";
+
+// document.querySelector("h1").style.borderRight = "9px solid red";
+
+// document.querySelector("h1").style.border = "6px solid black 9px solid red 3px solid green 5px solid blue";
